8a943da26ac5430b71d44dbedbe77f4a252d66f6
Klaudia Kriva <klaudia.kriva@akademiasovy.sk>
2019-11-25 09:29:49 +0100

    database helper, insert user, get userinfo

---
+++ app/src/main/java/com/example/instaliter/DatabaseHelper.java
@@ -0,0 +1,98 @@
+package com.example.instaliter;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.Date;
+
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public DatabaseHelper(Context context){
+        super(context, "instaliter.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table User (id integer primary key autoincrement," +
+                "name text, instaName text, email text, password text, cTime text)");
+        db.execSQL("create table UserInfo (idU integer primary key," +
+                "profilePhoto text, profileDescription text)");
+        db.execSQL("create table Follow (id integer primary key autoincrement," +
+                "followerID integer, followedID integer)");
+        db.execSQL("create table Comment (id integer primary key autoincrement," +
+                "idI integer, idU integer, commentText text, cTime text)");
+        db.execSQL("create table UserLike (id integer primary key autoincrement," +
+                "idU integer, idI integer)");
+        db.execSQL("create table ImageTag (id integer primary key autoincrement," +
+                "idT integer, idI integer)");
+        db.execSQL("create table Tag (id integer primary key autoincrement," +
+                "tagText text)");
+        db.execSQL("create table Image (id integer primary key autoincrement," +
+                " idU integer, path text, cTime text, decsriptipn text)"); //path ma byt text alebo blob?
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists User");
+        db.execSQL("drop table if exists UserInfo");
+        db.execSQL("drop table if exists Follow");
+        db.execSQL("drop table if exists Comment");
+        db.execSQL("drop table if exists UserLike");
+        db.execSQL("drop table if exists ImageTag");
+        db.execSQL("drop table if exists Tag");
+        db.execSQL("drop table if exists Image");
+        onCreate(db);
+    }
+
+    public boolean insertNewPost(int idU, String path, String description) {
+        if (path.equals("")) return false;
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("idU", idU);
+        contentValues.put("path", path);
+        contentValues.put("cTime", new Date().getTime()); //alebo iny time
+        contentValues.put("description", description);
+
+        long result = db.insert("Image", null, contentValues);
+
+        if (result == -1) return false;
+        else
+            return true;
+
+    }
+
+
+    public long insertNewUser(String name, String userName, String email, String password ){
+        if (name.equals("") || userName.equals("") || email.equals("") || password.equals("")) return -1;
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("name", name);
+        contentValues.put("instaName", userName);
+        contentValues.put("email", email);
+        contentValues.put("password", password); //ctime nie je potrebny
+
+        long result = db.insert("User", null, contentValues);
+
+        return result;
+
+    }
+
+
+    public boolean selectUserInfo(long userid){
+
+        return false;
+
+    } //na zaklade id
+    public boolean selectAllPosts(){return false;}
+    public boolean selectMyPosts(){return false;}
+
+
+
+
+
+
+}
--- app/src/main/java/com/example/instaliter/RegisterActivity.java
+++ app/src/main/java/com/example/instaliter/RegisterActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,20 +13,59 @@ import androidx.appcompat.app.AppCompatActivity;
 public class RegisterActivity extends AppCompatActivity {
 
     Button button;
+    DatabaseHelper databaseHelper;
+    EditText register_name;
+    EditText register_username;
+    EditText register_email;
+    EditText register_password;
+    public static long userID = 0;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_layout);
-        button = findViewById(R.id.btn_signUp2);
+        register_name = findViewById(R.id.register_name);
+        register_username = findViewById(R.id.register_username);
+        register_email = findViewById(R.id.register_email);
+        register_password = findViewById(R.id.register_password);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        databaseHelper = new DatabaseHelper(this);
+
+//        button = findViewById(R.id.btn_signUp2);
+//
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+////                insertNewUser();
+//                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+
+    }
+
+    public void insertNewUser(View v){
+        if(!(register_name.getText().toString().equals("") && register_username.getText().toString().equals("")
+        && register_email.getText().toString().equals("") && register_password.getText().toString().equals(""))){
+            long result = databaseHelper.insertNewUser(register_name.getText().toString(), register_username.getText().toString(),
+                    register_email.getText().toString(), register_password.getText().toString());
+            if (result != -1){
+                Toast.makeText(v.getContext(), "User inserted successfully",Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
                 startActivity(intent);
+                System.out.println(result + " result vracia");
+                //nastavit staticuser ako result
+                userID = result;
             }
-        });
+            else {
+                Toast.makeText(v.getContext(), "User not inserted",Toast.LENGTH_LONG).show();
+            }
+
+
+        }
+
+
 
     }
 }
